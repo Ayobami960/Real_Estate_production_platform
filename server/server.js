127b0083144a1b23eb47ed8d981ceb4870ed8bfc
Adetoye Kehinde <adetoye2021ayobami2@gmail.com>
2026-07-03 18:56:27 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ import connectDB from "./config/db.js";
 import router from "./routes/index.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // DB
 await connectDB();
